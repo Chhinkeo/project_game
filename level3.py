4fe1978420a9dcb0349befaa07a19d3fded2cebf
CHHINKEO.CHHOEUNH <CHHINKEO.CHHOEUNH@PNC.LAN>
2023-10-23 16:29:19 +0700

    edit some code

--- level3.py
+++ level3.py
@@ -60,10 +60,16 @@ play_left_file = Image.open("image/ninja_left.png")
 play_left_size = play_left_file.resize((55, 55))
 play_left = ImageTk.PhotoImage(play_left_size)
 
+#-----------------diamond------------------------------------
 d1_file = Image.open("image/diamond.png")
 d1_size = d1_file.resize((35,35))
 d1 = ImageTk.PhotoImage(d1_size)
 dm1 = canvas.create_image(490,560, image=d1)
+
+d2_file = Image.open("image/diamond.png")
+d2_size = d2_file.resize((35,35))
+d2 = ImageTk.PhotoImage(d2_size)
+dm2 = canvas.create_image(980,600, image=d2)
 
 #------------------grass block---------------------------------
 grass1_file = Image.open("image/grass.png")
@@ -76,12 +82,46 @@ grass2_size = grass2_file.resize((200,70))
 grass2 = ImageTk.PhotoImage(grass2_size)
 block_grass2 = canvas.create_image(450,595, image=grass2, tags = "PLATFORM")
 
+grass3_file = Image.open("image/grass.png")
+grass3_size = grass3_file.resize((100,70))
+grass3 = ImageTk.PhotoImage(grass3_size)
+block_grass3 = canvas.create_image(630,520, image=grass3, tags = "PLATFORM")
+
+grass4_file = Image.open("image/grass.png")
+grass4_size = grass4_file.resize((100,70))
+grass4 = ImageTk.PhotoImage(grass4_size)
+block_grass4 = canvas.create_image(750,585, image=grass4, tags = "PLATFORM")
+
+grass5_file = Image.open("image/grass.png")
+grass5_size = grass5_file.resize((150,70))
+grass5 = ImageTk.PhotoImage(grass5_size)
+block_grass5 = canvas.create_image(950,640, image=grass5, tags = "PLATFORM")
+
+grass6_file = Image.open("image/grass.png")
+grass6_size = grass6_file.resize((150,70))
+grass6 = ImageTk.PhotoImage(grass6_size)
+block_grass6 = canvas.create_image(1200,585, image=grass6, tags = "PLATFORM")
+
+grass7_file = Image.open("image/grass.png")
+grass7_size = grass7_file.resize((150,70))
+grass7 = ImageTk.PhotoImage(grass7_size)
+block_grass7 = canvas.create_image(1100,420, image=grass7, tags = "PLATFORM")
+
+grass8_file = Image.open("image/grass.png")
+grass8_size = grass8_file.resize((80,70))
+grass8 = ImageTk.PhotoImage(grass8_size)
+block_grass8 = canvas.create_image(1300,500, image=grass8, tags = "PLATFORM")
+
 #----------------challange------------------------------
 thorns1_file = Image.open("image/thorns.png")
 thorns1_size = thorns1_file.resize((200,40))
 thorns1 = ImageTk.PhotoImage(thorns1_size)
 canvas.create_image(350,675, image=thorns1)
 
+fire1_file = Image.open("Image/fire2.png")
+fire1_size = fire1_file.resize((50,50))
+fire1 = ImageTk.PhotoImage(fire1_size)
+canvas.create_image(905, 610, image = fire1)
 #-----------------function-------------------------------
 
 def check_movement(dx=0, dy=0, checkGround=False):
