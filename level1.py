27f6f3cfdf778dd75d2886270b70a17286c8d21a
NEARDEY.LOEM <NEARDEY.LOEM@PNC.LAN>
2023-10-23 10:23:56 +0700

    add file

--- level1.py
+++ level1.py
@@ -33,7 +33,7 @@ canvas.pack()
 #----Game_Level-1-----#
 canvas.create_rectangle(0,800,SCREEN_WIDTH, SCREEN_HEIGHT, fill="black", tags="PLATFORM")
 
-bg = Image.open("PIL/bg1.png")
+bg = Image.open("image/bg_level1.png")
 bg_size = bg.resize((SCREEN_WIDTH,SCREEN_HEIGHT))
 image = ImageTk.PhotoImage(bg_size)
 canvas.create_image(600,350, image=image)
@@ -63,14 +63,43 @@ play_left = ImageTk.PhotoImage(play_left_size)
 d1_file = Image.open("image/diamond.png")
 d1_size = d1_file.resize((35,35))
 d1 = ImageTk.PhotoImage(d1_size)
-dm1 = canvas.create_image(490,560, image=d1)
+dm1 = canvas.create_image(290,560, image=d1)
 
 #---------stone block----------#
-stone_file = Image.open("image/grass.png")
-stone_size = stone_file.resize((200,70))
-stone1 = ImageTk.PhotoImage(stone_file)
-block_stone1 = canvas.create_image(20,500, image=stone1, tags ="PLATFORM")
+grass1_file = Image.open("image/grass.png")
+grass_size = grass1_file.resize((200,70))
+grass1 = ImageTk.PhotoImage(grass1_file)
+block_grass1 = canvas.create_image(5,500, image=grass1, tags ="PLATFORM")
 
+grass2_file = Image.open("image/grass.png")
+grass2_size = grass2_file.resize((200,70))
+grass2 = ImageTk.PhotoImage(grass2_size)
+block_grass2 = canvas.create_image(300,600, image=grass2, tags = "PLATFORM")
+
+
+grass3_file = Image.open("image/grass.png")
+grass3_size = grass3_file.resize((200,70))
+grass3 = ImageTk.PhotoImage(grass3_size)
+block_grass3 = canvas.create_image(540,500, image=grass3, tags = "PLATFORM")
+
+
+grass4_file = Image.open("image/grass.png")
+grass4_size = grass4_file.resize((200,70))
+grass4 = ImageTk.PhotoImage(grass4_size)
+block_grass4 = canvas.create_image(790,420, image=grass4, tags = "PLATFORM")
+
+
+grass5_file = Image.open("image/grass.png")
+grass5_size = grass4_file.resize((200,70))
+grass5 = ImageTk.PhotoImage(grass5_size)
+block_grass5 = canvas.create_image(900,620, image=grass5, tags = "PLATFORM")
+
+
+#------challange---------
+fire1_file = Image.open("image/fire2.png")
+fire1_size = fire1_file.resize((60,70))
+fire1 = ImageTk.PhotoImage(fire1_size)
+canvas.create_image(370,545, image=fire1)
 
 
 
